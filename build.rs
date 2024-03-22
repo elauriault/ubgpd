26ca48f6f8c85dfb3969903749df67bc322862f4
Eric Lauriault <eric.lauriault@ubisoft.com>
2024-03-22 11:10:23 -0400

    netlink package updates

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut config = prost_build::Config::new();
     config.protoc_arg("--experimental_allow_proto3_optional");
-    tonic_build::configure().compile_with_config(config, &["proto/ubgp.proto"], &["protos"])?;
+    tonic_build::configure().compile_with_config(config, &["proto/ubgp.proto"], &["proto"])?;
     Ok(())
 }
